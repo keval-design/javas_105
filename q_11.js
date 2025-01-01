4581604a5aa3cbdc229bec0b60bd1b5939e7f1f8
keval-design <rathodkeval70901@gmail.com>
2025-01-01 23:09:53 +0530

    q_11 created

---
+++ q_11.js
@@ -0,0 +1,15 @@
+const N = 3;
+const M = 2;
+const arr = [
+  [1, 2],
+  [3, 4],
+  [5, 6]
+];
+
+for (let i = 0; i < N; i++) {
+  let row = "";
+  for (let j = 0; j < M; j++) {
+    row += arr[i][j] + " ";
+  }
+  console.log(row.trim());
+}
